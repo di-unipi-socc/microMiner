fd720fb22b178000ef621e2e23fac380897920e8
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-02-20 09:58:10 +0100

    Added capability to parse multiple yaml in one file

--- ArchMiner/Miner/StaticMiner/K8SParserContext.py
+++ ArchMiner/Miner/StaticMiner/K8SParserContext.py
@@ -18,18 +18,28 @@ class K8SParserContext:
     def addVersion(self, version: tuple(str, str), function: Callable([[dict], dict])):
         self.versions[version] = function
 
-    def parse(self, path: str):
+    def parse(self, path: str) -> []:
         if not(os.path.exists(path)):
             raise FileNotFoundError
+        nodesInfo = []
         if path.endswith('.json'):
             pass                           
         elif path.endswith('.yml'):
-            yaml = YAML(typ='safe')
-            content = yaml.load(Path(path))
-            try:
-                apiVersion = content['apiVersion'].split('/').pop()
-                nodeInfo = versions['yml', apiVersion](content)
-            except:
-                raise UnsupportedTypeError('Unsupported file')
+            yamlSplitted = self._splitFile(path)
+            loader = YAML(typ='safe')
+            for yaml in yamlSplitted:
+                content = loader.load(yaml)
+                try:
+                    apiVersion = content['apiVersion'].split('/').pop()
+                    nodesInfo.append(versions['yml', apiVersion](content))
+                except:
+                    raise UnsupportedTypeError('Unsupported file')
         else:
             raise UnsupportedTypeError('Unsupported type')
+        return nodesInfo
+
+    def _splitFile(self, path: str) -> []:
+        #TO-DO: Rendere più efficiente leggendo in blocchi
+        with open(path) as f:
+            text = f.read()
+        return text.split('---')
