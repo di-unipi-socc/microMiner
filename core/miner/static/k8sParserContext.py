076b027b343a7f4d9e5d9a53a5702d9571d0b8a9
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-02-26 20:19:36 +0100

    Added dynamic parser's class loading

--- core/miner/static/k8sParserContext.py
+++ core/miner/static/k8sParserContext.py
@@ -1,25 +1,25 @@
 import os.path
 import re
 import json
-from os import path
 from pathlib import Path
 from ruamel.yaml import YAML
 from typing import Optional
 from typing import Callable
+from .k8sParser import K8sParser
 from .v1Parser import V1Parser
 from .v1beta1Parser import V1beta1Parser
 from .v1beta2Parser import V1beta2Parser
 from ...errors import UnsupportedTypeError
 
-class K8SParserContext:
+class K8sParserContext:
     
-    def __init__(self, versions: Optional[dict] = {}):
+    def __init__(self, versions: dict):
         self.versions = versions
 
-    def addVersion(self, version: str, function: Callable([[dict, str], dict])):
-        self.versions[version] = function
+    def addVersion(self, version: str, parserClass: K8sParser):
+        self.versions[version] = parserClass
 
-    def parse(self, path: str) -> []:
+    def parse(self, path: Path) -> []:
         if not(os.path.exists(path)):
             raise FileNotFoundError
         nodesInfo = []
@@ -28,10 +28,10 @@ class K8SParserContext:
             contentDict = json.loads(contentStr)
             try:
                 apiVersion = contentDict['apiVersion'].split('/').pop()
-                function = versions[apiVersion]
+                parser = getClass(versions[apiVersion])
             except:
                 raise UnsupportedTypeError('Unsupported apiVersion')
-            nodesInfo.append(function(contentDict, contentStr))
+            nodesInfo.append(parser.parse(contentDict, contentStr))
         elif path.endswith('.yml'):
             yamlSplitted = self._readFile(path).split('---')
             loader = YAML(typ='safe')
@@ -39,10 +39,10 @@ class K8SParserContext:
                 content = loader.load(yaml)
                 try:
                     apiVersion = content['apiVersion'].split('/').pop()
-                    function = versions[apiVersion]
+                    parser = getClass(versions[apiVersion])
                 except:
                     raise UnsupportedTypeError('Unsupported apiVersion')
-                nodesInfo.append(function(content, yaml))
+                nodesInfo.append(parser.parse(contentDict, yaml))
         else:
             raise UnsupportedTypeError('Unsupported file type')
         return nodesInfo
@@ -51,3 +51,11 @@ class K8SParserContext:
         with open(path) as f:
             text = f.read()
         return text
+
+    def _get_class(self, fqnClass):
+        parts = fqnClass.split('.')
+        module = ".".join(parts[:-1])
+        m = __import__(module)
+        for comp in parts[1:]:
+            m = getattr(m, comp)            
+        return m
