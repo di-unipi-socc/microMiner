55b7397842f2eb38f141575524d5a8457f738cdc
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-02-28 18:50:54 +0100

    Added creation of nodes for pods, endpoints and services

--- plugins/kubernetes/miner/static/k8sStaticMiner.py
+++ plugins/kubernetes/miner/static/k8sStaticMiner.py
@@ -1,10 +1,10 @@
-from .staticMiner import StaticMiner
-from .k8sParserContext import K8sParserContext
+from .....core.topology.node import Node, Direction
+from .....core.topology.types import NodeType, RelationshipProperty
+from .....core.miner.static.staticMiner import StaticMiner
+from ...errors import StaticMinerError, WrongFolderError, UnsupportedTypeError, WrongFormatError
+from .parser.k8sParserContext import K8sParserContext
 from os import listdir
 from os.path import Path, isdir, isfile, join, exists
-from ...errors import StaticMinerError, WrongFolderError, UnsupportedTypeError, WrongFormatError
-from ...topology.node import Node
-from ...topology.types import NodeType, RelationshipProperty
 
 class K8sStaticMiner(StaticMiner):
 
@@ -15,39 +15,74 @@ class K8sStaticMiner(StaticMiner):
     def updateTopology(cls, info: dict, nodes: dict):
         files = cls._listFiles(info['folderPath'])
         parser = K8sParserContext(info['parserVersions'])
-        k8sObjects = []
-        for k8sFile in files:
+        parsedObjects = []
+
+        #Effettuo il parsing dei file di Kubernetes
+        for f in files:
             try:
-                k8sObjects.extend(parser.parse(k8sFile))
+                parsedObjects.extend(parser.parse(f))
             except UnsupportedTypeError:
-                print(k8sFile + 'is unsupported')
-        
-        for k8sObject in k8sObjects:
-            if k8sObject:
-                if k8sObject['type'] == 'pod':
-                    node = Node(k8sObject['info'])
-                    imageName = k8sObject['info']['image']
+                print(f + 'is unsupported')
+
+        #Recupero i pods e gli endpoints
+        for parsedObject in parsedObjects:
+            if parsedObject:
+                if parsedObject['type'] == 'pod':
+                    node = Node(parsedObject)
+                    imageName = parsedObject['info']['image']
                     if cls._isDatabase(imageName):
                         node.setType(NodeType.MICROTOSCA_NODES_DATABASE)
                     elif cls._isIngressController(imageName):
                         pass
-                    nodes[k8sObject['name']] = node
-                elif k8sObject['type'] == 'endpoints':
-                    endpoints = k8sObject['info']
-                    for endpoint in endpoints:
-                        name = endpoint.pop('name')
-                        nodes[name] = Node(endpoint)
-                elif k8sObject['type'] == 'service':
-                    #vanno creati gli archi con i nodi corrispondenti
-                    #se relativo ad un ingress non vanno inseriti nella topologia
-                    #altrimenti va inserito come message router (nell'insieme edge se è di tipo NodePort o LoadBalancer)
-                    pass
-                elif k8sObject['type'] == 'ingress':
-                    #cercare i service corrispondenti
-                    #cercare l'ingress controller corrispondente tramite l'annotation ingress.class (nel db il nome dell'ingress e poi nel dict degli ingress controller)
-                    #cercare i service corrispondenti
-                    #creare un unico nodo messageRouter nell'edge
-                    pass
+                    nodes[parsedObject['name']] = node
+                elif parsedObject['type'] == 'endpoints':
+                    for endpoint in parsedObject['info']:
+                        nodes[endpoint['name']] = Node(endpoint)
+
+        #Recupero i service
+        for parsedObject in parsedObjects:
+            if parsedObject['type'] == 'service':
+                serviceNode = Node(parsedObject)
+                if parsedObject['info']['selector']:
+                    #caso in cui il service abbia il selettore
+                    for nodeName, node in nodes:
+                        spec = node.getSpec()
+                        if spec['type'] == 'pod' and 'labels' in spec['info']:
+                            for key1, value1 in parsedObject['info']['selector']:
+                                found = False
+                                for key2, value2 in spec['info']['labels']:
+                                    if key1 == key2 and value1 == value2:
+                                        found = True
+                                        break
+                                if not found:
+                                    break
+                            if found:
+                                node.addEdge(parsedObject['name'], Direction.INCOMING)  
+                                serviceNode.addEdge(nodeName, Direction.OUTGOING)
+                else:
+                    #caso in cui il service sia senza selettore
+                    for nodeName, node in nodes:
+                        spec = node.getSpec()
+                        if spec['type'] == 'endpoint':
+                            found = False
+                            for endpointPort in spec['ports']:
+                                for servicePort in parsedObject['info']['ports']:
+                                    if endpointPort['name'] == servicePort['name'] and endpointPort['number'] == servicePort['targetPort']:
+                                        found = True
+                                        break
+                                if found:
+                                    node.addEdge(parsedObject['name'], Direction.INCOMING)  
+                                    serviceNode.addEdge(nodeName, Direction.OUTGOING)
+                                    break
+
+        #Recupero gli ingress
+        for parsedObject in parsedObjects:                    
+            if parsedObject['type'] == 'ingress':
+                #cercare i service corrispondenti
+                #cercare l'ingress controller corrispondente tramite l'annotation ingress.class (nel db il nome dell'ingress e poi nel dict degli ingress controller)
+                #cercare i service corrispondenti
+                #creare un unico nodo messageRouter nell'edge
+                pass
     
 
     @classmethod
