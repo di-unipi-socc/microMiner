b251d263f5632cb62846119a1b39ce90c133e1dc
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-03-10 18:41:36 +0100

    Implemented static miner context

--- core/miner/static/staticMinerContext.py
+++ core/miner/static/staticMinerContext.py
@@ -1,8 +1,20 @@
 import os.path
 from pathlib import Path
+from typing import Type
+from .staticMiner import StaticMiner
 
 class StaticMinerContext:
 
-    @staticmethod
-    def doStaticMining(config: dict, nodes: dict):
-        pass
+    @classmethod
+    def doStaticMining(cls, config: dict, nodes: dict):
+        staticMiner = cls._get_class(config['strategy'])
+        staticMiner.updateTopology(config['arguments'], nodes)
+    
+    @classmethod
+    def _get_class(cls, fqnClass: str) -> Type[StaticMiner]:
+        parts = fqnClass.split('.')
+        module = ".".join(parts[:-1])
+        m = __import__(module)
+        for comp in parts[1:]:
+            m = getattr(m, comp)            
+        return m
