664f23a40690dc4d28e7568908e1bfebff8feedf
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-03-23 19:12:29 +0100

    setup refactored

--- setup.py
+++ setup.py
@@ -3,3 +3,19 @@ setup(
     name = 'core',
     packages = find_packages(),
 )
+setup(
+    name = 'miner',
+    packages = find_packages(),
+)
+setup(
+    name = 'refiner',
+    packages = find_packages(),
+)
+setup(
+    name = 'exporter',
+    packages = find_packages(),
+)
+setup(
+    name = 'topology',
+    packages = find_packages(),
+)
