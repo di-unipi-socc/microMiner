295f452adc25b8a8b16af6aecce470f48a7164e4
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-03-23 19:17:36 +0100

    topology refactored

--- topology/node.py
+++ topology/node.py
@@ -34,14 +34,15 @@ class Node:
     def setIsEdge(self, isEdge: bool):
         self.isEdge = isEdge
 
-    def addEdge(self, nodeName: str, direction: Direction, communications: Optional[List[Communication]] = []):
-        if nodeName is None:
+    def addEdge(self, nodeName: str, direction: Direction, communications: Optional[List[Communication]] = [], relationshipProperties: Optional[List[RelationshipProperty]] = []):
+        if not nodeName:
             raise TypeError
 
         if direction is Direction.INCOMING:
             self.incomingEdges[nodeName] = communications
         elif direction is Direction.OUTGOING:
-            self.outgoingEdges[nodeName] = communications
+            edge = {'properties': relationshipProperties, 'communications': communications}
+            self.outgoingEdges[nodeName] = edge
         else:
             raise ValueError
 
@@ -52,9 +53,21 @@ class Node:
         if direction is Direction.INCOMING:
             self.incomingEdges[nodeName].append(communication)
         elif direction is Direction.OUTGOING:
-            self.outgoingEdges[nodeName].append(communication)
+            self.outgoingEdges[nodeName]['communications'].append(communication)
         else:
             raise ValueError
+
+    def addRelationshipProperty(self, nodeName: str, relationshipProperty: RelationshipProperty):
+        if not nodeName:
+            raise TypeError
+
+        self.outgoingEdges[nodeName]['properties'].append(relationshipProperty)
+
+    def getRelationshipProperties(self, nodeName: str):
+        if not nodeName:
+            raise TypeError
+
+        return self.outgoingEdges[nodeName]['properties']
     
     def getEdges(self, direction: Direction) -> dict:
         if direction is Direction.INCOMING:
