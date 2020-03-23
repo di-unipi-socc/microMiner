83636a5143e8ad1fb0dc338412ab7b04906cc4e9
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-03-23 19:12:17 +0100

    errors refactored

---
+++ errors.py
@@ -0,0 +1,6 @@
+class ArchMinerError(Exception):
+    """
+    A base class from which all other exceptions inherit.
+    If you want to catch all errors that the ArchMiner might raise,
+    catch this base exception.
+    """
--- miner/generic/errors.py
+++ miner/generic/errors.py
@@ -1,9 +1,4 @@
-class ArchMinerError(Exception):
-    """
-    A base class from which all other exceptions inherit.
-    If you want to catch all errors that the ArchMiner might raise,
-    catch this base exception.
-    """
+from errors import ArchMinerError
 
 class StaticMinerError(ArchMinerError):
     def __init__(self, message):
