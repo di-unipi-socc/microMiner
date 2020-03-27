6b215a4379846b9306b68b16d43f5cb7be986685
Giuseppe Muntoni <g.muntoni.cs@gmail.com>
2020-03-27 15:33:21 +0100

    refactoring

--- archMiner.py
+++ archMiner.py
@@ -15,22 +15,26 @@ def cli():
 @click.argument('source')
 @click.argument('target')
 @click.option('--time', default = 60, help = 'Seconds of monitoring')
-@click.option('--name', default = 'General application', help = 'Name of the microTOSCA model')
+@click.option('--name', default = 'Generic application', help = 'Name of the microTOSCA model')
 def generate(strategy, source, target, time, name):
     nodes = {}
     strategyConfig = Parser.searchMinerStrategy(strategy)
     if 'static' in strategyConfig:
         print('Executing static mining...')
-        StaticMinerContext.doStaticMining(strategyConfig['static']['class'], source, strategyConfig['static']['args'], nodes)
+        StaticMinerContext.doStaticMining(strategyConfig['static']['class'], source, strategyConfig['static']['args'] if 'args' in strategyConfig['static'] else {}, nodes)
     if 'dynamic' in strategyConfig:
         print('Executing dynamic mining...')
-        strategyConfig['dynamic']['args']['time'] = time
-        DynamicMinerContext.doDynamicMining(strategyConfig['dynamic']['class'], source, strategyConfig['dynamic']['args'], nodes)
+        if 'args' in strategyConfig['dynamic']:
+            strategyConfig['dynamic']['args']['time'] = time
+            DynamicMinerContext.doDynamicMining(strategyConfig['dynamic']['class'], source, strategyConfig['dynamic']['args'], nodes)
+        else:
+            strategyConfig['dynamic']['args'] = {'time': time}
+            DynamicMinerContext.doDynamicMining(strategyConfig['dynamic']['class'], source, strategyConfig['dynamic']['args'], nodes)
     
     #REFINER
 
     print('Exporting microTOSCA...')
-    YMLExporter.export(nodes, name, target)
+    YMLExporter.export(nodes, target, name)
 
 if __name__ == "__main__":
     cli()
