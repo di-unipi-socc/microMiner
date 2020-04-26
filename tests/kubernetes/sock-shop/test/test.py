59ce1043812b27f3cad08bc5ae2dce96af35ddf5
giuseppeMuntoni <g.muntoni.cs@gmail.com>
2020-04-26 19:22:29 +0000

    final test of sock-shop

--- tests/kubernetes/sock-shop/test/test.py
+++ tests/kubernetes/sock-shop/test/test.py
@@ -2,6 +2,6 @@ import time
 import os
 
 def runTest():
-    os.system('./tests/kubernetes/sock-shop/test/runLocust.sh -h 192.168.56.20:30001 -l ./tests/kubernetes/sock-shop/testLog -r 2')
+    os.system('./tests/kubernetes/sock-shop/test/runLocust.sh -h 192.168.56.20:30001 -l ./tests/kubernetes/sock-shop/testLog -r 2 1>/dev/null 2>/dev/null')
     time.sleep(3)
-    os.system('./tests/kubernetes/sock-shop/test/runLocust.sh -h 192.168.56.20:30001 -l ./tests/kubernetes/sock-shop/testLog -r 2')
+    os.system('./tests/kubernetes/sock-shop/test/runLocust.sh -h 192.168.56.20:30001 -l ./tests/kubernetes/sock-shop/testLog -r 2 1>/dev/null 2>/dev/null')
