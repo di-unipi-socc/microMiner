f4553f520f5bc522658a1112ebc6f74b2b1919af
giuseppeMuntoni <g.muntoni.cs@gmail.com>
2020-04-26 19:15:59 +0000

    improved efficiency and debugging

--- miner/strategies/kubernetes/dynamic/k8sDynamicMiner.py
+++ miner/strategies/kubernetes/dynamic/k8sDynamicMiner.py
@@ -16,14 +16,19 @@ from ruamel import yaml
 from ruamel.yaml import YAML
 from pathlib import Path
 from typing import Type
+import logging
+import copy
 import shutil
 import importlib
-import json
+import ijson.backends.yajl2_c as ijson
 import hashlib
 import time
 import os
 
 class K8sDynamicMiner(DynamicMiner):
+
+    log = logging.getLogger(__name__)
+    packetToPodHosts = {}
 
     def __init__(self):
         pass
@@ -35,122 +40,112 @@ class K8sDynamicMiner(DynamicMiner):
         k8sClient = client.ApiClient()
         loader = YAML(typ='safe')
         files = cls._listFiles(source)
-        newDeploymentPath = join(source, 'deploymentNew')
-        os.makedirs(newDeploymentPath)
         
-        #Deployment of the application
-        print('   Deploying the application...')
-        for k8sFile in files:
-            yamls = cls._readFile(k8sFile).split('---')
-            i = 0
-            for contentStr in yamls:
-                contentDict = loader.load(contentStr)
-                cls._prepareYaml(contentStr, contentDict, info['monitoringContainer'])
-                try:
-                    utils.create_from_dict(k8sClient, contentDict)
-                except utils.FailToCreateError:
-                    raise DeploymentError('Error deploying ' + k8sFile)
-                with open(join(newDeploymentPath, str(i) + '.yml'), 'w') as f:
-                    yamlContent = yaml.dump(contentDict)
-                    f.write(yamlContent)
-                i = i + 1
-        
-        #Wait until the deployment is completed
-        v1 = client.CoreV1Api()
-        deploymentCompleted = False
+        try:
+            #Deployment of the application
+            print('   Deploying the application...')
+            for k8sFile in files:
+                yamls = cls._readFile(k8sFile).split('---')
+                i = 0
+                for contentStr in yamls:
+                    contentDict = loader.load(contentStr)
+                    cls._prepareYaml(contentStr, contentDict, info['monitoringContainer'])
+                    with open(join(info['modDeploymentFiles'], str(i) + '.yml'), 'w') as f:
+                        try: 
+                            f.write(yaml.dump(contentDict))
+                            utils.create_from_dict(k8sClient, contentDict)
+                        except utils.FailToCreateError:
+                            cls._cleanEnvironment(info['modDeploymentFiles'], info['monitoringFiles'])
+                            raise DeploymentError('Error deploying ' + k8sFile)
+                    i = i + 1
+            
+            #Wait until the deployment is completed
+            v1 = client.CoreV1Api()
+            deploymentCompleted = False
 
-        while not deploymentCompleted:
+            while not deploymentCompleted:
+                pods = v1.list_pod_for_all_namespaces(watch = False)
+                deploymentCompleted = True
+                for pod in pods.items:
+                    if pod.spec.hostname in nodes:
+                        if pod.status.phase != 'Running' and pod.status.phase != 'Succeeded':
+                            deploymentCompleted = False
+                            break
+                if not deploymentCompleted:
+                    time.sleep(3)
+            time.sleep(10)
+            print('   Deployment completed')        
+
+            #Start monitoring
+            print('   Monitoring in progress...')   
             pods = v1.list_pod_for_all_namespaces(watch = False)
-            deploymentCompleted = True
+            containerName = ''.join(c for c in info['monitoringContainer'] if c.isalnum())
             for pod in pods.items:
                 if pod.spec.hostname in nodes:
-                    if pod.status.phase != 'Running' and pod.status.phase != 'Succeeded':
-                        deploymentCompleted = False
-                        break
-            if not deploymentCompleted:
-                time.sleep(3)
-        time.sleep(10)
-        print('   Deployment completed')        
-
-        #Start monitoring
-        print('   Monitoring in progress...')   
-        pods = v1.list_pod_for_all_namespaces(watch = False)
-        containerName = ''.join(c for c in info['monitoringContainer'] if c.isalnum())
-        for pod in pods.items:
-            if pod.spec.hostname in nodes:
-                filePath = join('/home/dump', pod.spec.hostname + '.json')
-                command = [
-                            './bin/sh',
-                            '-c',
-                            'tshark -i eth0 -a duration:' + str(info['time']) + ' -N nNdt -T json > ' + filePath + ' 2>/dev/null &']
+                    filePath = join('/home/dump', pod.spec.hostname + '.json')
+                    command = [
+                                './bin/sh',
+                                '-c',
+                                'tshark -i eth0 -a duration:' + str(info['time'] + 3) + ' -N nNdt -T json > ' + filePath + ' 2>/dev/null &']
+                    try:
+                        resp = stream(v1.connect_get_namespaced_pod_exec,
+                                                            pod.metadata.name, 
+                                                            pod.metadata.namespace, 
+                                                            command = command, 
+                                                            container = containerName,
+                                                            stderr=False, stdin=False,
+                                                            stdout=True, tty=False)
+                    except ApiException as e:
+                        cls._cleanEnvironment(info['modDeploymentFiles'], info['monitoringFiles'])
+                        raise MonitoringError(pod.metadata.name)
+            
+            #Start tests
+            time.sleep(3)
+            if info['test']:
                 try:
-                    resp = stream(v1.connect_get_namespaced_pod_exec,
-                                                        pod.metadata.name, 
-                                                        pod.metadata.namespace, 
-                                                        command = command, 
-                                                        container = containerName,
-                                                        stderr=False, stdin=False,
-                                                        stdout=True, tty=False)
-                except ApiException as e:
-                    raise MonitoringError(pod.metadata.name)
-        
-        #Start tests
-        time.sleep(0.5)
-        if info['test']:
-            try:
-                testModule = importlib.import_module(info['test'])
-                testModule.runTest()
-            except:
-                raise TestError('')
-        
-        #Wait until monitoring is finished
-        time.sleep(info['time'])
-        print('   Monitoring completed')
+                    testModule = importlib.import_module(info['test'])
+                    testModule.runTest()
+                except:
+                    cls._cleanEnvironment(info['modDeploymentFiles'], info['monitoringFiles'])
+                    raise TestError('')
+            
+            #Wait until monitoring is finished
+            time.sleep(info['time'])
+            print('   Monitoring completed')
 
-        #Save on local host the packets
-        pods = v1.list_pod_for_all_namespaces(watch = False)
-        for pod in pods.items:
-            if pod.spec.hostname in nodes:
-                remoteFilePath = join('home/dump', pod.spec.hostname + '.json')
-                localFilePath = join(info['monitoringFiles'], pod.spec.hostname + '.json')
-                os.system('kubectl cp -c ' + containerName + ' ' + pod.metadata.namespace + '/' + pod.metadata.name + ':' + remoteFilePath + ' ' + localFilePath)
+            #Save on local host the packets
+            pods = v1.list_pod_for_all_namespaces(watch = False)
+            for pod in pods.items:
+                if pod.spec.hostname in nodes:
+                    remoteFilePath = join('home/dump', pod.spec.hostname + '.json')
+                    localFilePath = join(info['monitoringFiles'], pod.spec.hostname + '.json')
+                    os.system('kubectl cp -c ' + containerName + ' ' + pod.metadata.namespace + '/' + pod.metadata.name + ':' + remoteFilePath + ' ' + localFilePath)
+            
+            #Create edges
+            print('   Analyzing packets...')
+            try:
+                files = cls._listFiles(info['monitoringFiles'])
+            except WrongFolderError:
+                cls._cleanEnvironment(info['modDeploymentFiles'], info['monitoringFiles'])
+                raise
+            for monitoringFilePath in files:
+                srcNodeName = monitoringFilePath.split('/')[-1].replace('.json', '')
+                with open(monitoringFilePath, 'rb') as monitoringFile:
+                    for packet in ijson.items(monitoringFile, 'item'):
+                        if cls._isOutgoingPacket(packet, nodes, srcNodeName):
+                            cls._createEdge(packet, nodes, srcNodeName)
+            
+            #Create communications
+            commFactory = ConcreteCommunicationFactory()
+            for monitoringFilePath in files:
+                srcNodeName = monitoringFilePath.split('/')[-1].replace('.json', '')
+                with open(monitoringFilePath, 'rb') as monitoringFile:
+                    for packet in ijson.items(monitoringFile, 'item'):
+                        if cls._isOutgoingPacket(packet, nodes, srcNodeName):
+                            cls._createCommunication(packet, nodes, commFactory, srcNodeName)
+        finally:
+            cls._cleanEnvironment(info['modDeploymentFiles'], info['monitoringFiles'])
         
-        #Create edges
-        files = cls._listFiles(info['monitoringFiles'])
-        for monitoringFile in files:
-            srcNodeName = monitoringFile.split('/')[-1].replace('.json', '')
-            fileContent = cls._readFile(monitoringFile)
-            if fileContent:
-                packetList = json.loads(fileContent)
-                for packet in packetList:
-                    if cls._isOutgoingPacket(packet, nodes, srcNodeName):
-                        cls._createEdge(packet, nodes, srcNodeName)
-        
-        dump = {}
-        for nodeName, node in nodes.items():
-            dump[nodeName] = node.dump()
-        with open('./dump.yml', 'w') as f:
-            dumpContent = yaml.dump(dump)
-            f.write(dumpContent)
-        
-        #Create communications
-        commFactory = ConcreteCommunicationFactory()
-        for monitoringFile in files:
-            srcNodeName = monitoringFile.split('/')[-1].replace('.json', '')
-            fileContent = cls._readFile(monitoringFile)
-            #os.system('rm ' + monitoringFile)
-            if fileContent:
-                packetList = json.loads(fileContent)
-                for packet in packetList:
-                    if cls._isOutgoingPacket(packet, nodes, srcNodeName):
-                        cls._createCommunication(packet, nodes, commFactory, srcNodeName)
-        
-        #Clean the environment
-        print('   Cleaning the environment...')
-        files = cls._listFiles(newDeploymentPath)
-        for yamlFile in files:
-            os.system('kubectl delete -f ' + yamlFile + ' 1>/dev/null 2>/dev/null')
-        shutil.rmtree(newDeploymentPath)
 
     @classmethod
     def _readFile(cls, path: str) -> str:
@@ -179,7 +174,7 @@ class K8sDynamicMiner(DynamicMiner):
         if podSpec:
             if not 'hostname' in podSpec:
                 podSpec['hostname'] = hashlib.sha1(contentStr.encode('utf-8')).hexdigest()
-            print(contentDict['metadata']['name'] + ':' + podSpec['hostname'])
+            cls.log.info(contentDict['metadata']['name'] + ':' + podSpec['hostname'])
             podSpec['containers'].append({'name': ''.join(c for c in imageName if c.isalnum()), 'image': imageName})
 
     @classmethod
@@ -196,11 +191,14 @@ class K8sDynamicMiner(DynamicMiner):
         return True
 
     @classmethod
-    def _getPodHostname(cls, ip: str) -> str:
+    def _getPodHostname(cls, ip: str, host: str) -> str:
+        if host in cls.packetToPodHosts:
+            return cls.packetToPodHosts[host]
         v1 = client.CoreV1Api()
         pods = v1.list_pod_for_all_namespaces(watch = False)
         for pod in pods.items:
             if pod.status.pod_ip == ip:
+                cls.packetToPodHosts[host] = pod.spec.hostname
                 return pod.spec.hostname
         return ''
 
@@ -213,8 +211,7 @@ class K8sDynamicMiner(DynamicMiner):
         if 'svc' in dstNodeName.split('.'):
             dstNodeName = dstNodeName.split('.svc.')[0] + '.svc'
         if not dstNodeName in nodes:
-            dstNodeName = cls._getPodHostname(packetLayers['ip']['ip.dst'])
-            #print(dstNodeName)
+            dstNodeName = cls._getPodHostname(packetLayers['ip']['ip.dst'], dstNodeName)
         if not dstNodeName in nodes:
             return ('', None)
 
@@ -255,16 +252,15 @@ class K8sDynamicMiner(DynamicMiner):
         dstNode = dst[1]
         if not dstNodeName:
             return
-        #print(dstNodeName + '==' + dstNode.getFrontendName())
         packet['_source']['layers']['ip']['ip.dst_host'] = dstNodeName
         dstIsService = dstNode.getType() == NodeType.MICROTOSCA_NODES_MESSAGE_ROUTER
-        communication = commFactory.build(packet)
+        communication = commFactory.build(copy.deepcopy(packet))
         if communication:
             try:
                 srcNode.addCommunication(dstNodeName, communication)
                 dstNode.addCommunication(srcNodeName, communication)
             except EdgeNotExistsError:
-                print('not exist edge: ' + srcNodeName + '-> ' + dstNodeName)
+                cls.log.warn('Edge (' + srcNodeName + ', ' + dstNodeName + ') not exists')
                 pass
         
         if dstIsService:
@@ -272,10 +268,17 @@ class K8sDynamicMiner(DynamicMiner):
             edges = dstNode.getEdges(Direction.OUTGOING)
             for adjacentName in edges.keys():
                 packet['_source']['layers']['ip']['ip.dst_host'] = adjacentName
-                communication = commFactory.build(packet)
+                communication = commFactory.build(copy.deepcopy(packet))
                 if communication:
-                    try:
-                        dstNode.addCommunication(adjacentName, communication)
-                        nodes[adjacentName].addCommunication(dstNodeName, communication)
-                    except EdgeNotExistsError:
-                        pass
+                    dstNode.addCommunication(adjacentName, communication)
+                    nodes[adjacentName].addCommunication(dstNodeName, communication)
+    
+    @classmethod
+    def _cleanEnvironment(cls, modDeploymentFiles: str, monitoringFiles: str):
+        files = cls._listFiles(modDeploymentFiles)
+        for yamlFile in files:
+            os.system('kubectl delete -f ' + yamlFile + ' 1>/dev/null 2>/dev/null')
+            os.remove(yamlFile)
+        files = cls._listFiles(monitoringFiles)
+        for monitoringFile in files:
+            os.remove(monitoringFile)
